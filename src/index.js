6784d650f9676302c9e534772408c8e7b6fb73d8
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-09 18:41:39 +0700

    Test out send message function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+"use strict"
+
+const dotenv = require("dotenv");
+const Discord = require("discord.js");
+
+dotenv.config();
+const client = new Discord.Client({ intents: [ "GUILDS", "GUILD_MESSAGES" ]});
+
+client.on("ready", async () => await onStartup());
+
+async function onStartup() {
+  console.log("SnipeID is now running.");
+
+  const channel = await client.channels.cache.get(process.env.CHANNEL_ID);
+  await channel.send("Hello, world!");
+}
+
+client.login(process.env.BOT_TOKEN);
