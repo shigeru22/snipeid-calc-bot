d47b311f32780dcdb1ed03b05142845afb57c78d
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-09 21:28:56 +0700

    Add more padding to padStart()

--- src/utils/counter.js
+++ src/utils/counter.js
@@ -5,11 +5,11 @@ function counter(top_1, top_8, top_15, top_25, top_50, username) {
   const draft = new Discord.MessageEmbed();
   draft.setTitle("Points for "+ username + ":");
   draft.setDescription("```"
-  + ((top_1*5).toString().padEnd(6)) + "= " + (+ top_1 +" x 5\n").toString().padStart(18)
-  + (((top_8 - top_1)*3).toString().padEnd(6)) + "= " + ("("+ top_8 +" - "+ top_1 +") x 3\n").toString().padStart(18)
-  + (((top_15 - top_8)*2).toString().padEnd(6)) +"= " + ("("+ top_15 +" - "+ top_8 +") x 2\n").toString().padStart(18)
-  + ((top_25 - top_15).toString().padEnd(6)) +"= " + ("("+ top_25 +" - " + top_15 + ") x 1\n").toString().padStart(18)
-  + ((top_50 - top_25).toString().padEnd(6)) +"= " + ("("+ top_50 +" - " + top_25 + ") x 1\n```").toString().padStart(18)
+  + ((top_1*5).toString().padEnd(6)) + "= " + (+ top_1 +" x 5").toString().padStart(19) + "\n"
+  + (((top_8 - top_1)*3).toString().padEnd(6)) + "= " + ("("+ top_8 +" - "+ top_1 +") x 3").toString().padStart(19) + "\n"
+  + (((top_15 - top_8)*2).toString().padEnd(6)) +"= " + ("("+ top_15 +" - "+ top_8 +") x 2").toString().padStart(19) + "\n"
+  + ((top_25 - top_15).toString().padEnd(6)) +"= " + ("("+ top_25 +" - " + top_15 + ") x 1").toString().padStart(19) + "\n"
+  + ((top_50 - top_25).toString().padEnd(6)) +"= " + ("("+ top_50 +" - " + top_25 + ") x 1").toString().padStart(19) + "\n```"
   + "\n= **" + points + "** points.");
   
   draft.setColor("#ff0000");
