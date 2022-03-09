1cb4628a036528a6ea6b5cd7cdb7f4d13b767933
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-09 21:04:10 +0700

    Implement Bathbot description parsing

--- src/index.js
+++ src/index.js
@@ -3,19 +3,49 @@
 const dotenv = require("dotenv");
 const Discord = require("discord.js");
 const counter = require("./utils/counter.js");
+const parser = require("./utils/parser.js");
 
 dotenv.config();
 const client = new Discord.Client({ intents: [ "GUILDS", "GUILD_MESSAGES" ]});
 
+const BATHBOT_USER_ID = "297073686916366336";
+
 client.on("ready", async () => await onStartup());
+client.on("messageCreate", async (msg) => await onNewMessage(msg))
 
 async function onStartup() {
   console.log("SnipeID is now running.");
 
   const channel = await client.channels.cache.get(process.env.CHANNEL_ID);
-  const draft = counter(33, 477, 876, 1378, 2234, "Venta");
+}
 
-  console.log(draft.description);
+async function onNewMessage(msg) {
+  const channel = await client.channels.cache.get(process.env.CHANNEL_ID);
+
+  if(msg.channelId === process.env.CHANNEL_ID) {
+    if(msg.author.id === BATHBOT_USER_ID) {
+      // await channel.send("Calculating score...");
+      const embeds = msg.embeds; // always 0
+      const len = embeds.length;
+      const index = embeds.findIndex(
+        embed => embed.title.toLowerCase()
+          .startsWith("in how many top x map leaderboards is")
+      );
+
+      if(index === -1) {
+        return;
+      }
+
+      console.log("[LOG] Calculating points data.");
+
+      const desc = embeds[index].description;
+
+      // [ top_1, top_8, top_15, top_25, top_50 ]
+      const topCounts = parser(desc);
+
+      console.log(topCounts);
+    }
+  }
 }
 
 client.login(process.env.BOT_TOKEN);
---
+++ src/utils/parser.js
@@ -0,0 +1,25 @@
+function parseTopCountDescription(desc) {
+  const res = desc.replace(/```(\n)*/g, "");
+  const tops = res.split("\n");
+
+  const len = tops.length;
+  for(let i = 0; i < len; i++) {
+    const temp = tops[i];
+    
+    tops[i] = temp.replace(/Top /g, "");
+    tops[i] = tops[i].replace(/ /g, "");
+    tops[i] = tops[i].replace(/,/g, "");
+  }
+
+  const topsArray = [];
+  tops.forEach(item => {
+    if(item !== "") {
+      const temp = item.split(":");
+      topsArray.push(parseInt(temp[1], 10));
+    }
+  });
+
+  return topsArray;
+}
+
+module.exports = parseTopCountDescription;
