22d8cf6a0699af638aed51d2c0ca758b36071be6
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-15 17:33:33 +0700

    Implement user ID retrieval function

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,8 +6,11 @@ const DatabaseErrors = {
   TYPE_ERROR: 2,
   DUPLICATED_DISCORD_ID: 3,
   DUPLICATED_OSU_ID: 4,
-  CLIENT_ERROR: 5
+  NOT_FOUND: 5,
+  CLIENT_ERROR: 6
 };
+
+/* user operations */
 
 async function insertUser(pool, discordId, osuId) {
   if(!(pool instanceof Pool)) {
@@ -78,7 +81,60 @@ async function insertUser(pool, discordId, osuId) {
   }
 }
 
+async function getDiscordUserByOsuId(pool, osuId) {
+  if(!(pool instanceof Pool)) {
+    console.log("[ERROR] getDiscordUserByOsuId :: pool must be a Pool object instance.");
+    return DatabaseErrors.TYPE_ERROR;
+  }
+
+  if(typeof(osuId) !== "number") {
+    console.log("[ERROR] getDiscordUserByOsuId :: osuId must be number.");
+    return DatabaseErrors.TYPE_ERROR;
+  }
+
+  const selectQuery = "SELECT * FROM users WHERE osuId=$1";
+  const selectValues = [ osuId ];
+
+  try {
+    const client = await pool.connect();
+
+    const discordUserResult = await client.query(selectQuery, selectValues);
+    client.release();
+
+    if(typeof(discordUserResult.rows[0]) === "undefined")  {
+      return DatabaseErrors.NOT_FOUND;
+    }
+
+    if(discordUserResult.rows[0].osuid !== osuId) {
+      return DatabaseErrors.CLIENT_ERROR;
+    }
+
+    return {
+      userId: discordUserResult.rows[0].userid,
+      discordId: discordUserResult.rows[0].discordid,
+      osuId: discordUserResult.rows[0].osuid
+    };
+  }
+  catch (e) {
+    if(e instanceof Error) {
+      if(e.code === "ECONNREFUSED") {
+        console.log("[ERROR] insertUser :: Database connection failed.");
+        return DatabaseErrors.CONNECTION_ERROR;
+      }
+      else {
+        console.log("[ERROR] insertUser :: An error occurred while inserting user: " + e.message);
+      }
+    }
+    else {
+      console.log("[ERROR] insertUser :: Unknown error occurred.");
+    }
+
+    return DatabaseErrors.CLIENT_ERROR;
+  }
+}
+
 module.exports = {
   DatabaseErrors,
-  insertUser
+  insertUser,
+  getDiscordUserByOsuId
 };
