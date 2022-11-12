bea15675f5e7906c7e12185385f690d5132eb1b2
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-11-12 21:34:36 +0700

    Limit temporary time variable scoping

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -15,29 +15,39 @@ class TimeUtils {
    * @returns { string } Delta time in string representation.
    */
   static deltaTimeToString(ms: number): string {
-    const years = Math.floor(ms / 31536000000);
-    if(years >= 1) {
-      return `${ years.toString() } year${ years !== 1 ? "s" : "" }`;
+    {
+      const years = Math.floor(ms / 31536000000);
+      if(years >= 1) {
+        return `${ years.toString() } year${ years !== 1 ? "s" : "" }`;
+      }
     }
 
-    const months = Math.floor(ms / 2592000000);
-    if(months >= 1) {
-      return `${ months.toString() } month${ months !== 1 ? "s" : "" }`;
+    {
+      const months = Math.floor(ms / 2592000000);
+      if(months >= 1) {
+        return `${ months.toString() } month${ months !== 1 ? "s" : "" }`;
+      }
     }
 
-    const days = Math.floor(ms / 86400000);
-    if(days >= 1) {
-      return `${ days.toString() } day${ days !== 1 ? "s" : "" }`;
+    {
+      const days = Math.floor(ms / 86400000);
+      if(days >= 1) {
+        return `${ days.toString() } day${ days !== 1 ? "s" : "" }`;
+      }
     }
 
-    const hours = Math.floor(ms / 3600000);
-    if(hours >= 1) {
-      return `${ hours.toString() } hour${ hours !== 1 ? "s" : "" }`;
+    {
+      const hours = Math.floor(ms / 3600000);
+      if(hours >= 1) {
+        return `${ hours.toString() } hour${ hours !== 1 ? "s" : "" }`;
+      }
     }
 
-    const minutes = Math.floor(ms / 60000);
-    if(minutes >= 1) {
-      return `${ minutes.toString() } minute${ minutes !== 1 ? "s" : "" }`;
+    {
+      const minutes = Math.floor(ms / 60000);
+      if(minutes >= 1) {
+        return `${ minutes.toString() } minute${ minutes !== 1 ? "s" : "" }`;
+      }
     }
 
     const seconds = Math.floor(ms / 1000);
