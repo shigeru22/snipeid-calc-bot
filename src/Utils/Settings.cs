60fde6396ce36e45182707ea979b06075a7e93dd
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-01 10:48:42 +0700

    utils: Create settings class

---
+++ src/Utils/Settings.cs
@@ -0,0 +1,62 @@
+using System.Text.Json;
+
+namespace LeaderpointsBot.Utils;
+
+public class Settings
+{
+	private static readonly string DEFAULT_SETTINGS_PATH = "appsettings.json";
+
+	public static class SettingsTypes
+	{
+		public struct JsonClientSettings
+		{
+			public string BotToken { get; set; }
+			public int LogSeverity { get; set; }
+		}
+
+		public struct JsonDatabaseSettings
+		{
+			public string HostName { get; set; }
+			public int Port { get; set; }
+			public string Username { get; set; }
+			public string Password { get; set; }
+			public string DatabaseName { get; set; }
+			public string CAFilePath { get; set; }
+		}
+
+		public struct JsonOsuClientSettings
+		{
+			public int ClientId { get; set; }
+			public string ClientSecret { get; set; }
+		}
+
+		public struct JsonSettings
+		{
+			public JsonClientSettings Client { get; set; }
+			public JsonDatabaseSettings Database { get; set; }
+			public JsonOsuClientSettings OsuClient { get; set; }
+		}
+	}
+
+	public SettingsTypes.JsonClientSettings Client { get; private set; }
+	public SettingsTypes.JsonDatabaseSettings Database { get; private set; }
+	public SettingsTypes.JsonOsuClientSettings OsuClient { get; private set; }
+
+	private static readonly Settings instance = new(DEFAULT_SETTINGS_PATH);
+
+	public static Settings Instance { get => instance; }
+
+	private Settings(string settingsPath)
+	{
+		JsonSerializerOptions options = new()
+		{
+			PropertyNameCaseInsensitive = true
+		};
+
+		SettingsTypes.JsonSettings temp = JsonSerializer.Deserialize<SettingsTypes.JsonSettings>(File.ReadAllText(settingsPath), options);
+
+		Client = temp.Client;
+		Database = temp.Database;
+		OsuClient = temp.OsuClient;
+	}
+}
