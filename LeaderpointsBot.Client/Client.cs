19ac4cec8d58b710fef3a8a2ef3a524c9ecead96
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-02 10:46:20 +0700

    client: Verbose start event logging

--- LeaderpointsBot.Client/Client.cs
+++ LeaderpointsBot.Client/Client.cs
@@ -15,13 +15,18 @@ public class Client
 		this.botToken = botToken;
 
 		client.Log += Log.Write;
+
+		Log.WriteVerbose("Client", "Client initialized using botToken parameter.");
 	}
 
 	public async Task Run()
 	{
+		await Log.WriteVerbose("Run", "Start client using specified botToken.");
+
 		await client.LoginAsync(Discord.TokenType.Bot, botToken);
 		await client.StartAsync();
 
+		await Log.WriteVerbose("Run", "Client started. Awaiting process indefinitely.");
 		await Task.Delay(-1);
 	}
 }
