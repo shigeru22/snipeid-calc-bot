0e909d6a9148b6746d4112ff0713ae8a86de55e5
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-03 20:56:29 +0700

    database-config: Re-add config property setters

--- LeaderpointsBot.Database/src/Config.cs
+++ LeaderpointsBot.Database/src/Config.cs
@@ -2,12 +2,12 @@ namespace LeaderpointsBot.Database;
 
 public struct DatabaseConfig
 {
-	public string HostName { get; }
-	public int Port { get; }
-	public string Username { get; }
-	public string Password { get; }
-	public string DatabaseName { get; }
-	public string? CAFilePath { get; }
+	public string HostName { get; set; }
+	public int Port { get; set; }
+	public string Username { get; set; }
+	public string Password { get; set; }
+	public string DatabaseName { get; set; }
+	public string? CAFilePath { get; set; }
 
 	public DatabaseConfig(string hostName, int port, string username, string password, string databaseName, string? caFilePath)
     {
