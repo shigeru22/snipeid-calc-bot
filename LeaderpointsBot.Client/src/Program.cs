d14914852d006c6116e9abd7e624499ff7ea8abe
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-04 15:46:14 +0700

    client: Setup instances before starting client

--- LeaderpointsBot.Client/src/Program.cs
+++ LeaderpointsBot.Client/src/Program.cs
@@ -1,3 +1,6 @@
+using LeaderpointsBot.Api;
+using LeaderpointsBot.Api.OsuStats;
+using LeaderpointsBot.Database;
 using LeaderpointsBot.Utils;
 
 namespace LeaderpointsBot.Client;
@@ -6,6 +9,27 @@ public static class Program
 {
 	public static async Task Main(string[] args)
 	{
+		await Log.WriteInfo("Main", "Program started.");
+
+		await Log.WriteVerbose("Main", "Setting up ApiFactory.");
+
+		ApiFactory.Instance.OsuApiInstance.Token.ClientID = Settings.Instance.OsuApi.ClientID;
+		ApiFactory.Instance.OsuApiInstance.Token.ClientSecret = Settings.Instance.OsuApi.ClientSecret;
+
+		await Log.WriteVerbose("Main", "Setting up DatabaseFactory.");
+
+		DatabaseFactory.Instance.SetConfig(new DatabaseConfig()
+		{
+			HostName = Settings.Instance.Database.HostName,
+			Port = Settings.Instance.Database.Port,
+			Username = Settings.Instance.Database.Username,
+			Password = Settings.Instance.Database.Password,
+			DatabaseName = Settings.Instance.Database.DatabaseName,
+			CAFilePath = Settings.Instance.Database.CAFilePath,
+		});
+
+		await Log.WriteVerbose("Main", "Starting up client.");
+
 		Client client = new(Settings.Instance.Client.BotToken);
 		await client.Run();
 	}
