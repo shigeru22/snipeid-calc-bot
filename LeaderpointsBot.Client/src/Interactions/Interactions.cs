0ebe0f5b0cf39fa0b50ce0014c7874ce40c4629e
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-04 16:58:34 +0700

    interactions: Prototype and debug interactions

--- LeaderpointsBot.Client/src/Interactions/Interactions.cs
+++ LeaderpointsBot.Client/src/Interactions/Interactions.cs
@@ -18,11 +18,51 @@ public class InteractionsFactory
 
 	public async Task OnInvokeSlashInteraction(SocketSlashCommand cmd)
 	{
-		await Log.WriteDebug("OnInvokeSlashInteraction", $"Slash interaction from { cmd.User.Username }#{ cmd.User.Discriminator }: { cmd.Data.Name } ({ cmd.Data.Id })");
+		// await Log.WriteDebug("OnInvokeSlashInteraction", $"Slash interaction from { cmd.User.Username }#{ cmd.User.Discriminator }: { cmd.Data.Name } ({ cmd.Data.Id })");
+
+		switch(cmd.Data.Name)
+		{
+			case "link":
+				// TODO: link user
+				await Log.WriteDebug("OnInvokeSlashInteraction", "Link user command received.");
+				break;
+			case "ping":
+				// TODO: send ping
+				await Log.WriteDebug("OnInvokeSlashInteraction", "Send ping command received.");
+				break;
+			case "count":
+				// TODO: count points
+				await Log.WriteDebug("OnInvokeSlashInteraction", "Count points command received.");
+				break;
+			case "whatif":
+				// TODO: count what-if points
+				await Log.WriteDebug("OnInvokeSlashInteraction", "Count what-if points command received.");
+				break;
+			case "serverleaderboard":
+				// TODO: send server leaderboard
+				await Log.WriteDebug("OnInvokeSlashInteraction", "Get server leaderboard command received.");
+				break;
+			case "config":
+				// TODO: configure server settings
+				await Log.WriteDebug("OnInvokeSlashInteraction", "Configuration command received.");
+				break;
+			case "help":
+				// TODO: send help message
+				await Log.WriteDebug("OnInvokeSlashInteraction", "Send help message command received.");
+				break;
+		}
 	}
 
 	public async Task OnInvokeUserContextInteraction(SocketUserCommand cmd)
 	{
-		await Log.WriteDebug("OnInvokeContextInteraction", $"Context interaction from { cmd.User.Username }#{ cmd.User.Discriminator }: { cmd.Data.Name } ({ cmd.Data.Id })");
+		// await Log.WriteDebug("OnInvokeContextInteraction", $"Context interaction from { cmd.User.Username }#{ cmd.User.Discriminator }: { cmd.Data.Name } ({ cmd.Data.Id })");
+
+		switch(cmd.Data.Name)
+		{
+			case "Calculate points":
+				// TODO: count points
+				await Log.WriteDebug("OnInvokeUserContextInteraction", "Count points command received.");
+				break;
+		}
 	}
 }
--- LeaderpointsBot.Client/src/Messages/Messages.cs
+++ LeaderpointsBot.Client/src/Messages/Messages.cs
@@ -20,6 +20,71 @@ public class MessagesFactory
 
 	public async Task OnNewMessage(SocketMessage msg)
 	{
-		await Log.WriteDebug("OnNewMessage", $"Message from { msg.Author.Username }#{ msg.Author.Discriminator }: { msg.Content }");
+		if(msg.Channel.GetChannelType() != ChannelType.Text)
+		{
+			return;
+		}
+
+		// await Log.WriteDebug("OnNewMessage", $"Message from { msg.Author.Username }#{ msg.Author.Discriminator }: { msg.Content }");
+
+		string[] contents = new Regex("\\s+").Split(msg.Content);
+		bool isClientMentioned = msg.MentionedUsers.Count(user => user.Id == client.CurrentUser.Id) == 1 && contents[0].Contains(client.CurrentUser.Id.ToString());
+
+		bool processed = await HandleCommands(msg);
+
+		if(!processed && isClientMentioned)
+		{
+			// TODO: send random conversation message
+			await Log.WriteDebug("OnNewMessage", "Bot mentioned but nothing processed. Send random message.");
+		}
+	}
+
+	private async Task<bool> HandleCommands(SocketMessage msg)
+	{
+		string[] contents = new Regex("\\s+").Split(msg.Content);
+		if(contents.Length < 2)
+		{
+			return false;
+		}
+
+		bool processed = true;
+
+		switch(contents[1])
+		{
+			case "link":
+				// TODO: verify user
+				await Log.WriteDebug("HandleCommands", "Link user command received.");
+				break;
+			case "ping":
+				// TODO: send ping
+				await Log.WriteDebug("HandleCommands", "Send ping command received.");
+				break;
+			case "count":
+				// TODO: count points
+				await Log.WriteDebug("HandleCommands", "Count points command received.");
+				break;
+			case "whatif":
+				// TODO: count what-if points
+				await Log.WriteDebug("HandleCommands", "Count what-if points command received.");
+				break;
+			case "lb": // fallthrough
+			case "leaderboard":
+				// TODO: send server leaderboard
+				await Log.WriteDebug("HandleCommands", "Get server leaderboard command received.");
+				break;
+			case "config":
+				// TODO: configure server settings
+				await Log.WriteDebug("HandleCommands", "Configuration command received.");
+				break;
+			case "help":
+				// TODO: send help message
+				await Log.WriteDebug("HandleCommands", "Send help message command received.");
+				break;
+			default:
+				processed = false;
+				break;
+		}
+
+		return processed;
 	}
 }
