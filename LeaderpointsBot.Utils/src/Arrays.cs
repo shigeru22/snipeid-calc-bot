b913b1a1837b54e85f5d35dcae22207ae87639e7
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-19 10:07:57 +0700

    utils: Create array operations class

---
+++ LeaderpointsBot.Utils/src/Arrays.cs
@@ -0,0 +1,44 @@
+namespace LeaderpointsBot.Utils;
+
+public static class ArrayOperations
+{
+	public static int[] SearchRankAtTopsArray(int[,] topsCount, IReadOnlyList<int> ranks)
+	{
+		List<int> tempTops = new();
+
+		int ranksLength = ranks.Count;
+		for(int i = 0; i < ranksLength; i++)
+		{
+			tempTops.Add(SearchRankArray(topsCount, ranks[i]));
+		}
+
+		return tempTops.ToArray();
+	}
+
+	private static int SearchRankArray(int[,] topsCount, int rank)
+	{
+		int min = 0;
+		int max = topsCount.Length - 1;
+
+		// binary search anyone?
+		while (min <= max) {
+			int mid = (min + max) / 2;
+
+			if (rank == topsCount[mid, 0])
+			{
+				return topsCount[mid, 1];
+			}
+
+			if (rank < topsCount[mid, 0])
+			{
+				max = mid - 1;
+			}
+			else
+			{
+				min = mid + 1;
+			}
+		}
+
+		throw new KeyNotFoundException("No ranks found in topsCount array.");
+	}
+}
