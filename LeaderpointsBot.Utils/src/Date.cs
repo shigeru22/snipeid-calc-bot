7afe7c58e86676f3af14aea18eb1150bc473db1f
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-19 10:09:09 +0700

    utils: Use TimeSpan for delta calculation

--- LeaderpointsBot.Utils/src/Date.cs
+++ LeaderpointsBot.Utils/src/Date.cs
@@ -2,6 +2,8 @@ namespace LeaderpointsBot.Utils;
 
 public static class Date
 {
+	private static readonly DateTime zeroTime = new(1, 1, 1);
+
 	public static string GetCurrentDate(bool useUtc = true)
 	{
 		DateTime currentTime = useUtc ? DateTime.UtcNow : DateTime.Now;
@@ -18,5 +20,37 @@ public static class Date
 	{
 		DateTime currentTime = useUtc ? DateTime.UtcNow : DateTime.Now;
 		return $"{ currentTime.Year }/{ currentTime.Month.ToString().PadLeft(2, '0') }/{ currentTime.Day.ToString().PadLeft(2, '0') } { currentTime.Hour.ToString().PadLeft(2, '0') }:{ currentTime.Minute.ToString().PadLeft(2, '0') }:{ currentTime.Second.ToString().PadLeft(2, '0') }";
+	}
+
+	public static string DeltaTimeToString(TimeSpan time)
+	{
+		DateTime deltaTime = zeroTime - time;
+
+		if(deltaTime.Year - 1 >= 1)
+		{
+			return $"{ deltaTime.Year - 1 } year{ (deltaTime.Year - 1 != 1 ? "s" : "") }";
+		}
+
+		if(deltaTime.Month - 1 >= 1)
+		{
+			return $"{ deltaTime.Month } month{ (deltaTime.Month - 1 != 1 ? "s" : "") }";
+		}
+
+		if(deltaTime.DayOfYear - 1 >= 1)
+		{
+			return $"{ deltaTime.DayOfYear } day{ (deltaTime.DayOfYear - 1 != 1 ? "s" : "") }";
+		}
+
+		if(deltaTime.Hour - 1 >= 1)
+		{
+			return $"{ deltaTime.Hour - 1 } hour{ (deltaTime.Hour - 1 != 1 ? "s" : "") }";
+		}
+
+		if(deltaTime.Minute >= 1)
+		{
+			return $"{ deltaTime.Minute - 1 } month{ (deltaTime.Minute - 1 != 1 ? "s" : "") }";
+		}
+		
+		return $"{ deltaTime.Second - 1 } second{ (deltaTime.Second - 1 != 1 ? "s" : "") }";
 	}
 }
