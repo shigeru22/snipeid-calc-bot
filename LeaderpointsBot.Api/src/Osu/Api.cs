7b219cc1a50a285162c48bc70554f36c46c159d3
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-21 21:26:26 +0700

    api: Add get by username and use key parameter

--- LeaderpointsBot.Api/src/Osu/Api.cs
+++ LeaderpointsBot.Api/src/Osu/Api.cs
@@ -46,7 +46,41 @@ public class OsuApi
 			client.DefaultRequestHeaders.Add("Authorization", $"Bearer { await Token.GetTokenAsync() }");
 
 			await Log.WriteVerbose("GetUserByOsuID", "Requesting osu!api user endpoint.");
-			response = await client.GetAsync($"{ OsuApi.OSU_API_ENDPOINT }/users/{ osuId }");
+			response = await client.GetAsync($"{ OsuApi.OSU_API_ENDPOINT }/users/{ osuId }?type=id");
+		}
+		catch (Exception e)
+		{
+			await Log.WriteError("GetUserByOsuID", $"An unhandled error occurred while revoking token. Exception details below.\n{ e }");
+			throw new ApiInstanceException("Unhandled exception.");
+		}
+
+		if(response.StatusCode != HttpStatusCode.OK) // not 200
+		{
+			await Log.WriteWarning("GetUserByOsuID", $"osu!api returned status code { (int)response.StatusCode }");
+			throw new ApiResponseException(response.StatusCode);
+		}
+
+		OsuDataTypes.OsuApiUserResponseData ret = JsonSerializer.Deserialize<OsuDataTypes.OsuApiUserResponseRawData>(await response.Content.ReadAsStringAsync()).ToStandardData();
+
+		await Log.WriteVerbose("GetUserByOsuID", "osu! user retrieved successfully. Returning data as OsuApiUserResponseData object.");
+		return ret;
+	}
+
+	public async Task<OsuDataTypes.OsuApiUserResponseData> GetUserByOsuUsername(string osuUsername)
+	{
+		await Log.WriteVerbose("GetUserByOsuID", $"Requesting osu! user with username { osuUsername }.");
+
+		HttpResponseMessage response;
+
+		try
+		{
+			using HttpClient client = new();
+
+			client.DefaultRequestHeaders.Accept.Add(new("application/json"));
+			client.DefaultRequestHeaders.Add("Authorization", $"Bearer { await Token.GetTokenAsync() }");
+
+			await Log.WriteVerbose("GetUserByOsuID", "Requesting osu!api user endpoint.");
+			response = await client.GetAsync($"{ OsuApi.OSU_API_ENDPOINT }/users/{ osuUsername }?key=username");
 		}
 		catch (Exception e)
 		{
