db2630ec2a848f799b9a30548b21ad8f1e000f31
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-01-21 21:07:19 +0700

    utils: Create List overloaded counter methods

--- LeaderpointsBot.Client/src/Embeds/Counter.cs
+++ LeaderpointsBot.Client/src/Embeds/Counter.cs
@@ -30,7 +30,46 @@ public static class Counter
 		else
 		{
 			// TODO: create respektive warning settings option
-			description = "```\n" + 
+			description = "```\n" +
+				$"{ tops[0] * 5, -6 } = { $"{ tops[0] } x 5", 19 }\n" +
+				$"{ (tops[1] - tops[0]) * 3, -6 } = { $"({ tops[1] } - { tops[0] }) x 3", 19 }\n" +
+				$"{ tops[2] - tops[1], -6 } = { $"({ tops[2] } - { tops[1] }) x 1", 19 }\n" +
+				$"{ tops[3] - tops[2], -6 } = { $"({ tops[3] } - { tops[2] }) x 1", 19 }\n" +
+				"```\n" +
+				$"= **{ points }** points.";
+		}
+
+		return new EmbedBuilder().WithTitle($"Points for {osuUsername}:")
+			.WithDescription(description)
+			.WithColor(238, 229, 229)
+			.Build();
+	}
+
+	public static Embed CreateCountEmbed(string osuUsername, List<int[]> topsCount, bool useRespektive = false)
+	{
+		int points = CalculateTopPoints(topsCount, useRespektive);
+		string description;
+
+		// since CalculateTopPoints() throw exception on invalid data,
+		// these arrays could be assured without rechecking
+		int[] ranks = !useRespektive ? new[] { 1, 8, 15, 25, 50 } : new[] { 1, 8, 25, 50 };
+		int[] tops = ArrayOperations.SearchRankAtTopsArray(topsCount, ranks);
+
+		if(!useRespektive)
+		{
+			description = "```\n" +
+				$"{ tops[0] * 5, -6 } = { $"{ tops[0] } x 5", 19 }\n" +
+				$"{ (tops[1] - tops[0]) * 3, -6 } = { $"({ tops[1] } - { tops[0] }) x 3", 19 }\n" +
+				$"{ (tops[2] - tops[1]) * 2, -6 } = { $"({ tops[2] } - { tops[1] }) x 2", 19 }\n" +
+				$"{ tops[3] - tops[2], -6 } = { $"({ tops[3] } - { tops[2] }) x 1", 19 }\n" +
+				$"{ tops[4] - tops[3], -6 } = { $"({ tops[4] } - { tops[3] }) x 1", 19 }\n" +
+				"```\n" +
+				$"= **{ points }** points.";
+		}
+		else
+		{
+			// TODO: create respektive warning settings option
+			description = "```\n" +
 				$"{ tops[0] * 5, -6 } = { $"{ tops[0] } x 5", 19 }\n" +
 				$"{ (tops[1] - tops[0]) * 3, -6 } = { $"({ tops[1] } - { tops[0] }) x 3", 19 }\n" +
 				$"{ tops[2] - tops[1], -6 } = { $"({ tops[2] } - { tops[1] }) x 1", 19 }\n" +
@@ -46,6 +85,23 @@ public static class Counter
 	}
 
 	public static int CalculateTopPoints(int[,] topsCount, bool useRespektive = false)
+	{
+		int[] ranks = !useRespektive ? new[] { 1, 8, 15, 25, 50 } : new[] { 1, 8, 25, 50 };
+		int[] tops = ArrayOperations.SearchRankAtTopsArray(topsCount, ranks);
+
+		switch(useRespektive)
+		{
+			case false when tops.Length != 5: // fallthrough
+			case true when tops.Length != 4:
+				throw new InvalidEmbedDescriptionException();
+			case true: // respektive
+				return tops[0] * 5 + (tops[1] - tops[0]) * 3 + (tops[2] - tops[1]) + (tops[3] - tops[2]);
+			default: // osu!stats
+				return tops[0] * 5 + (tops[1] - tops[0]) * 3 + (tops[2] - tops[1]) * 2 + (tops[3] - tops[2]) + (tops[4] - tops[3]);
+		}
+	}
+
+	public static int CalculateTopPoints(List<int[]> topsCount, bool useRespektive = false)
 	{
 		int[] ranks = !useRespektive ? new[] { 1, 8, 15, 25, 50 } : new[] { 1, 8, 25, 50 };
 		int[] tops = ArrayOperations.SearchRankAtTopsArray(topsCount, ranks);
--- LeaderpointsBot.Utils/src/Arrays.cs
+++ LeaderpointsBot.Utils/src/Arrays.cs
@@ -15,6 +15,19 @@ public static class ArrayOperations
 		return tempTops.ToArray();
 	}
 
+	public static int[] SearchRankAtTopsArray(List<int[]> topsCount, IReadOnlyList<int> ranks)
+	{
+		List<int> tempTops = new();
+
+		int ranksLength = ranks.Count;
+		for(int i = 0; i < ranksLength; i++)
+		{
+			tempTops.Add(SearchRankArray(topsCount, ranks[i]));
+		}
+
+		return tempTops.ToArray();
+	}
+
 	private static int SearchRankArray(int[,] topsCount, int rank)
 	{
 		int min = 0;
@@ -40,5 +53,32 @@ public static class ArrayOperations
 		}
 
 		throw new KeyNotFoundException("No ranks found in topsCount array.");
+	}
+
+	private static int SearchRankArray(List<int[]> topsCount, int rank)
+	{
+		int min = 0;
+		int max = topsCount.Count - 1;
+
+		// binary search anyone?
+		while (min <= max) {
+			int mid = (min + max) / 2;
+
+			if (rank == topsCount[mid][0])
+			{
+				return topsCount[mid][1];
+			}
+
+			if (rank < topsCount[mid][0])
+			{
+				max = mid - 1;
+			}
+			else
+			{
+				min = mid + 1;
+			}
+		}
+
+		throw new KeyNotFoundException("No ranks found in topsCount list.");
 	}
 }
